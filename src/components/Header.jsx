e821eecf00119feb4cae94adf1f521cd7de31dbc
hho114 <hho114@csu.fullerton.edu>
2020-08-17 16:37:24 -0700

    setup and route project with static

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CodeIcon from '@material-ui/icons/Code';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <CodeIcon />
+         Decode Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
